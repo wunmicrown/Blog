0da21a8c92fb49d07d1d1b6201ab2cc1c6ab64c0
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-03-15 15:23:00 +0100

    setup my pages routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Components/pages/Home'
+import Register from './Components/pages/auth/Register'
+import Login from './Components/pages/auth/Login'
+import Write from './Components/pages/Write'
 
 const App = () => {
     return (
         <>
-            <div className=' text-blue-500 text-center mt-20'>
-                App
-                
-            </div>
-
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/register' element={<Register />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/write' element={<Write />} />
+            </Routes>
         </>
     )
 }
---
+++ src/Components/pages/auth/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <>
+        <div>This is the Login page</div>
+    </>
+  )
+}
+
+export default Login

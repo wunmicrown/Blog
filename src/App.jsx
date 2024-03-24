3370b568715fbac94cfb962d78264f01334f9457
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-03-24 18:09:22 +0100

    added a link to list item

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,7 @@ const App = () => {
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
                     <Route path="/write" element={<Write/>} />
+                    {/* <Route path="/posts" element={<Posts/>} /> */}
 
                 </Route>
             </Routes>
--- src/Components/Navbar/PublicNavbar.jsx
+++ src/Components/Navbar/PublicNavbar.jsx
@@ -1,19 +1,28 @@
 import React from 'react'
 import logoImg from '../../assets/img/blog-logo.png'
+import { Link } from 'react-router-dom'
 const PublicNavbar = () => {
   return (
     <>
-      <nav className=' bg-red-400 text-white shadow-2xl w-full fixed top-0 flex  justify-between px-6 p-4'>
+      <nav className=' bg-slate-50 text-black shadow-2xl w-full fixed top-0 flex  justify-between px-6 p-4'>
       <div>
       <img src={logoImg} alt="" className=' w-14'/>
       </div>
-      <ul className=' flex flex-shrink-0 gap-2 text-center pt-4'>
+      <ul className=' flex flex-shrink-0 gap-8   text-center pt-4 font-bold'>
           <li>
           </li>
-          <li>Home</li>
-          <li>Posts</li>
-          <li>Login</li>
-          <li>Register</li>
+          <li>
+            <Link to ={'/'} >Home</Link>
+            </li>
+          <li>
+            <Link to ={'/posts'}>Posts</Link>
+          </li>
+          <li>
+            <Link to ={'/login'}>Login</Link>
+          </li>
+          <li>
+            <Link to ={'/register'}>Register</Link>
+          </li>
       </ul>
       <ul>
         profile

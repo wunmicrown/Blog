f2ca19619848bea3e0d97ea254b6af81540f11f7
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-03-26 13:38:16 +0100

    added a constant variable folder

---
+++ src/Components/constants/Api.js
@@ -0,0 +1,10 @@
+// export const API_URL='https://learnification.onrender.com'     
+export const API_URL='http://localhost:4000'     
+
+
+// export const API_URL= import.meta.env.MODE === "development"? 'http://localhost:8000' : "https://auth-backend-dufd.onrender.com"
+// export const API_URL = import.meta.env.PROD? "https://auth-backend-dufd.onrender.com" : "http://localhost:8000"
+// console.log(
+//     import.meta.env.MODE,
+//     import.meta.env.PROD,
+// );
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -5,7 +5,7 @@ const Homepage = () => {
   return (
     <>
       <Category/>
-
+      
     </>
   )
 }

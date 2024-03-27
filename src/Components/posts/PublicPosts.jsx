cdbc5aeec884af92f35a4acf205ea773e1bc6647
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-03-27 11:33:59 +0100

    changes were on PublicPosts file

--- src/Components/posts/PublicPosts.jsx
+++ src/Components/posts/PublicPosts.jsx
@@ -60,7 +60,7 @@ const PublicPosts = () => {
                       </Link>
                       <div className="mb-4">
                         <span className="inline-block py-1 px-3 text-xs leading-5 text-green-500 hover:text-green-600 font-medium uppercase bg-green-100 hover:bg-green-200 rounded-full shadow-sm">
-                          {post?.category?.name}
+                          {post.author}
                         </span>
                       </div>
                       <p className="mb-2 text-coolGray-500 font-medium">

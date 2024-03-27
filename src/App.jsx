cfaf0ccac539d392d24a5c25f2b9f6670ca9e88f
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-03-27 16:59:19 +0100

    Changes were made App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Login from './pages/auth/Login'
-import Write from './pages/Write'
-import Homepage from './pages/Homepage'
-import Register from './pages/auth/Register'
-import { ToastContainer } from 'react-toastify'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/auth/Login';
+import Write from './pages/Write';
+import Homepage from './pages/Homepage';
+import Register from './pages/auth/Register';
+import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
-import VerifyEmail from './pages/auth/VerifyEmail'
+import VerifyEmail from './pages/auth/VerifyEmail';
 import Layout from './components/layout/Layout';
 
 const App = () => {
-
     return (
         <>
             <ToastContainer
@@ -26,21 +25,17 @@ const App = () => {
                 theme="light"
             />
             <Routes>
-                <Route path='/' element={<Layout />}>
-                    
+                <Route element={<Layout />}>
                     <Route index element={<Homepage />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
                     <Route path="/verify-email" element={<VerifyEmail />} />
                     <Route path="/write" element={<Write />} />
                     {/* <Route path="/posts" element={<Posts/>} /> */}
-
                 </Route>
             </Routes>
-
-
         </>
-    )
-}
+    );
+};
 
-export default App
+export default App;
--- src/Components/layout/Layout.jsx
+++ src/Components/layout/Layout.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import PublicNavbar from '../Navbar/PublicNavbar'
 import Footer from '../Footer'
-// import PublicNavbar from '../Navbar/PublicNavbar'
-// import Footer from '../Footer'
-// import PrivateNavbar from '../components/Navbar/PrivateNavbar'
+
 
 const Layout = () => {
   return (

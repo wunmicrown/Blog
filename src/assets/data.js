edc2c7f147573d3d278d4a01b893db1c8e5ac5c6
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-03-27 11:30:40 +0100

    changes were made at data js file

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,11 +1,8 @@
 import cover1 from '../assets/imgs/ca3.png'
-import cover2 from '../assets/imgs/ca5.png'
 import cover3 from '../assets/imgs/ca6.png'
-import cover4 from '../assets/imgs/ca7.png'
-import cover5 from '../assets/imgs/ca8.png'
-import cover6 from '../assets/imgs/ca3.png'
+import cover6 from '../assets/imgs/b9.jpg'
 import cover7 from '../assets/imgs/ca7.png'
-import cover8 from '../assets/imgs/ca5.png'
+import cover8 from '../assets/imgs/b6.jpg'
 export const category = [
     
   {
@@ -15,30 +12,15 @@ export const category = [
     cover:cover1,
   },
   
-    {
-      id: 6,
-      category: "Fashion",
-      title: "Becoming a Dragonfly",
-      cover:cover2,
-    },
+   
     {
       id: 4,
-      category: "Travel",
+      category: "Javascript",
       title: "There's always light at the end of the tunnel",
       cover: cover3
     },
-    {
-      id: 3,
-      category: "Sport",
-      title: "Stay Calm And Surf",
-      cover:cover4,
-    },
-    {
-      id: 5,
-      category: "Fun",
-      title: "There's always light at the end of the tunnel",
-      cover:cover5,
-    },
+    
+ 
     {
       id: 2,
       category: "Web devlopment",
@@ -47,13 +29,13 @@ export const category = [
     },
     {
       id: 7,
-      category: "Business",
+      category: "Technology",
       title: "Stay Calm And Surf",
       cover:cover7,
     },
     {
       id: 8,
-      category: "Life",
+      category: "Javascript",
       title: "Stay Calm And Surf",
       cover:cover8
     },

d36b35d4807d2fbfe0a76d20c7a5aeca45c51855
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-03-27 12:44:41 +0100

    Added a register schema

---
+++ src/Components/validationSchema/registerSchema.js
@@ -0,0 +1,22 @@
+import * as yup from "yup";
+
+export const validationSchema = yup.object({
+  username: yup
+    .string()
+    .required("Last name is required")
+    .min(4, "Minimum of four characters")
+    .max(50, "Maximum of 50 characters"),
+
+  email: yup.string()
+    .required("Email is required")
+    .email("Invalid email address"),
+
+  password: yup
+    .string()
+    .required("Password is required")
+    .matches(
+      /^[a-zA-Z0-9]{8,}$/,
+      "Password must be at least 8 characters long and contain only letters and numbers"
+    ),
+
+});

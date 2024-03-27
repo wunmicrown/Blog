b8bdc1df0109caccb2706cf41e3465729c2b3718
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-03-27 16:29:41 +0100

    added a folder to the layout component

--- src/Components/layout/Layout.jsx
+++ src/Components/layout/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import PublicNavbar from '../components/Navbar/PublicNavbar'
-import Footer from '../components/Footer'
+import PublicNavbar from '../Navbar/PublicNavbar'
+import Footer from '../Footer'
 // import PrivateNavbar from '../components/Navbar/PrivateNavbar'
 
 const Layout = () => {

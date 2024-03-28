ab1431558874b67cd2bb46ac66be391b5d755914
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-03-28 16:55:36 +0100

    Changes were made to Layout Component

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,22 +1,21 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import PublicNavbar from './Navbar/PublicNavbar'
-import PrivateNavbar from './Navbar/PrivateNavbar'
-import Footer from './Footer'
-
+import React, { useState } from 'react';
+import { Outlet } from 'react-router-dom';
+import PublicNavbar from './Navbar/PublicNavbar';
+import PrivateNavbar from './Navbar/PrivateNavbar';
+import Footer from './Footer';
 
 const Layout = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // Set initial state to false for not logged in
+
   return (
     <>
       <main>
-        {/* <PublicNavbar /> */}
-        <PrivateNavbar />
+        {isLoggedIn ? <PrivateNavbar /> : <PublicNavbar />}
         <Outlet />
         <Footer />
-
       </main>
     </>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;

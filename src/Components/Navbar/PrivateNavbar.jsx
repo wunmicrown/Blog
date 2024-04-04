9f3fd058441ab2c00668fcfec32290d104cf298e
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-04-04 12:50:32 +0100

    Changes were made to PriateNavbar

--- src/Components/Navbar/PrivateNavbar.jsx
+++ src/Components/Navbar/PrivateNavbar.jsx
@@ -152,7 +152,7 @@ const PrivateNavbar = (
                           <Menu.Item>
                             {({ active }) => (
                               <Link
-                                to={"/dashboard"}
+                                to={"/user-profile-details"}
                                 className={`${active ? "bg-gray-100" : ""
                                   } block px-4 py-2 text-sm text-gray-700 hover:text-green-500 hover:underline`}
                               >

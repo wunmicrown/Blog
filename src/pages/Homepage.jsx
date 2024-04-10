ef88bc6dddddb04e9d10bf6ed16663ce29233437
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-04-10 21:46:18 +0100

    Folder was removed from Category component

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Category from '../components/category/Category'
+import Category from '../components/Category'
 import PublicPosts from './posts/PublicPosts'
 
 const Homepage = () => {

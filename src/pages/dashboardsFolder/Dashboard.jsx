10ecd4f81ebae549ad452d8e2ba33962bf240824
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-04-12 17:13:35 +0100

    formatted  code to improve readability

--- src/pages/dashboardsFolder/Dashboard.jsx
+++ src/pages/dashboardsFolder/Dashboard.jsx
@@ -27,7 +27,7 @@ const Dashboard = ({ postId, initialCommentsCount }) => {
         const response = await axios.get(`${API_URL}/api/v1/posts`, {
           params: { category_id: categoryName },
         });
-        console.log("Posts", response);
+        // console.log("Posts", response);
         setPosts(response.data.posts);
         setLoading(false);
       } catch (error) {
@@ -200,7 +200,7 @@ const Dashboard = ({ postId, initialCommentsCount }) => {
                   </div>
                 ))
               )}
-              
+
             </div>
           </div>
         </section>

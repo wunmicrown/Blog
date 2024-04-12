f8c2f62503d2c9bd5124cd99994f5d2694c09469
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-04-12 17:19:15 +0100

    formatted  code to improve readability

--- src/pages/settings/ChangeEmail.jsx
+++ src/pages/settings/ChangeEmail.jsx
@@ -43,49 +43,49 @@ const ChangeEmail = () => {
   };
 
   return (
-    
-      <>
 
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 pt-28 ">
-                <div className="w-auto p-6 bg-white rounded-xl shadow-md">
-                <FaBlog className=" text-green-500 h-16 mx-auto w-full mt-8 mb-8" />
-                    <p className="text-gray-600 text-center mb-4">
-                    Change your email address and we'll send you an <span className="text-green-600 font-bold">OTP</span> to verify your email
-                    </p>
-                    <div>
-                        <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                           Change email
-                        </label>
-                        <div className="mb-6 relative">
-                            <AiOutlineMail className="absolute text-gray-500 text-2xl top-2 left-2" />
+    <>
 
-                            <input
-                                name="email"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                                type="email"
-                                placeholder="Enter your email"
-                                className="pl-10 pr-4 py-2 w-full border border-gray-300 rounded-lg focus:outline-none focus:border-green-500"
-                            />
-                        </div>
-                    </div>
-                    {loading ? (
-                        <Loading />
-                    ) : (
-                        <button
-                            className="w-full px-4 py-2 text-white bg-green-500 rounded-lg hover:bg-green-600 focus:outline-none"
-                            onClick={handleSubmit}
-                            disabled={loading}
-                        >
-                            Change
-                        </button>
-                    )}
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 pt-28 ">
+        <div className="w-auto p-6 bg-white rounded-xl shadow-md">
+          <FaBlog className=" text-green-500 h-16 mx-auto w-full mt-8 mb-8" />
+          <p className="text-gray-600 text-center mb-4">
+            Change your email address and we'll send you an <span className="text-green-600 font-bold">OTP</span> to verify your email
+          </p>
+          <div>
+            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+              Change email
+            </label>
+            <div className="mb-6 relative">
+              <AiOutlineMail className="absolute text-gray-500 text-2xl top-2 left-2" />
 
-                    <ChangePassword/>
-                </div>
-                <Uploadpic/>
+              <input
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                placeholder="Enter your email"
+                className="pl-10 pr-4 py-2 w-full border border-gray-300 rounded-lg focus:outline-none focus:border-green-500"
+              />
             </div>
-      </>
+          </div>
+          {loading ? (
+            <Loading />
+          ) : (
+            <button
+              className="w-full px-4 py-2 text-white bg-green-500 rounded-lg hover:bg-green-600 focus:outline-none"
+              onClick={handleSubmit}
+              disabled={loading}
+            >
+              Change
+            </button>
+          )}
+
+          <ChangePassword />
+        </div>
+        <Uploadpic />
+      </div>
+    </>
   );
 };
 

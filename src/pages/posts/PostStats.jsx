33c8f909640e12c0073925c27f4e4c12b8c80ec2
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-04-12 17:18:18 +0100

    formatted  code to improve readability

--- src/pages/posts/PostStats.jsx
+++ src/pages/posts/PostStats.jsx
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { API_URL } from "../constants/Api";
 import useCommentsCount from "../comments/useCommentsCount";
+import { toast } from "react-toastify";
 
 const PostStats = ({ postId, initialLikes, initialDislikes, initialCommentsCount }) => {
   const [likes, setLikes] = useState(initialLikes || 0);
   const [dislikes, setDislikes] = useState(initialDislikes || 0);
-  const [userLiked, setUserLiked] = useState(false); 
+  const [userLiked, setUserLiked] = useState(false);
   const commentsCount = useCommentsCount(postId, URL, initialCommentsCount);
-  console.log("commentCount", commentsCount);
   useEffect(() => {
     // Check if the user has liked the post when the component mounts
     const checkUserLiked = () => {
@@ -33,7 +33,7 @@ const PostStats = ({ postId, initialLikes, initialDislikes, initialCommentsCount
             Authorization: `Bearer ${token}`,
           },
         });
-        console.log("Liked response", response);
+        // console.log("Liked response", response);
         if (response.status === 200) {
           setLikes(likes + 1);
           setUserLiked(true);
@@ -52,7 +52,7 @@ const PostStats = ({ postId, initialLikes, initialDislikes, initialCommentsCount
         console.error("User is not authenticated.");
         return;
       }
-  
+
       const response = await axios.put(`${API_URL}/api/v1/posts/dislikes/${postId}`, null, {
         headers: {
           Authorization: `Bearer ${token}`,
@@ -78,10 +78,11 @@ const PostStats = ({ postId, initialLikes, initialDislikes, initialCommentsCount
           Authorization: `Bearer ${token}`,
         },
       });
-      
+
       if (response.status === 200) {
         // Call a function to refresh comments count or update UI as needed
-        console.log("Comment deleted successfully");
+        toast.success("Comment deleted successfully")
+        // console.log("Comment deleted successfully");
       }
     } catch (error) {
       console.error("Error deleting comment:", error);
@@ -110,7 +111,7 @@ const PostStats = ({ postId, initialLikes, initialDislikes, initialCommentsCount
           </path>
         </svg>0
       </button>
-      <button className="flex items-center gap-1 m-2 text-2xl text-gray-400" 
+      <button className="flex items-center gap-1 m-2 text-2xl text-gray-400"
         onClick={dislikePostHandler}
       >
         {/* Dislike icon */}
@@ -121,18 +122,18 @@ const PostStats = ({ postId, initialLikes, initialDislikes, initialCommentsCount
             d="M7.5 15h2.25m8.024-9.75c.011.05.028.1.052.148.591 1.2.924 2.55.924 3.977a8.96 8.96 0 01-.999 4.125m.023-8.25c-.076-.365.183-.75.575-.75h.908c.889 0 1.713.518 1.972 1.368.339 1.11.521 2.287.521 3.507 0 1.553-.295 3.036-.831 4.398C20.613 14.547 19.833 15 19 15h-1.053c-.472 0-.745-.556-.5-.96a8.95 8.95 0 00.303-.54m.023-8.25H16.48a4.5 4.5 0 01-1.423-.23l-3.114-1.04a4.5 4.5 0 00-1.423-.23H6.504c-.618 0-1.217.247-1.605.729A11.95 11.95 0 002.25 12c0 .434.023.863.068 1.285C2.427 14.306 3.346 15 4.372 15h3.126c.618 0 .991.724.725 1.282A7.471 7.471 0 007.5 19.5a2.25 2.25 0 002.25 2.25.75.75 0 00.75-.75v-.633c0-.573.11-1.14.322-1.672.304-.76.93-1.33 1.653-1.715a9.04 9.04 0 002.86-2.4c.498-.634 1.226-1.08 2.032-1.08h.384"></path>
         </svg>0
       </button>
-      
+
       <div
         className="flex items-center gap-1 m-2 text-2xl text-gray-400">
-          {/* Comment icon */}
-          <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth="1.5"
-        stroke="currentColor"
-        className="w-6 h-6"
-      >
+        {/* Comment icon */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
@@ -143,9 +144,9 @@ const PostStats = ({ postId, initialLikes, initialDislikes, initialCommentsCount
         </svg>
         {commentsCount}comment
       </div>
-      
+
     </div>
-   
+
   );
 };
 

f4b34bbe1a811ae372e68d48da9db6b1d909b8fe
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-04-15 21:32:38 +0100

    make a changes to CreatePost component

--- src/pages/posts/CreatePosts.jsx
+++ src/pages/posts/CreatePosts.jsx
@@ -1,13 +1,17 @@
 import React, { useState, useEffect, useRef } from "react";
 import JoditEditor from "jodit-react";
 import { toast } from "react-toastify";
-import { Card, CardBody, Form, Input, Container, Button } from 'reactstrap';
+import { Card, CardBody, Form, Input, Container, Button, Placeholder } from 'reactstrap';
 import axios from 'axios';
 import { API_URL } from "../constants/Api";
-import { Md30FpsSelect, MdFileUpload } from "react-icons/md";
+import { MdFileUpload } from "react-icons/md";
 
 const CreatePosts = () => {
   const editor = useRef(null);
+  const [content, setContent] = useState('');
+
+
+
   const [categories, setCategories] = useState([]);
   const [user, setUser] = useState('');
   const [image, setImage] = useState('');
@@ -106,80 +110,81 @@ const CreatePosts = () => {
   }
 
   return (
-    <div className="wrapper h-screen">
-      <Card className="shadow-sm border-0 mt-2 bg-[#F5F5F6]  w-full">
-        <CardBody>
-          <Form onSubmit={createPost}>
-            <div className="pt-28 flex justify-end mr-16">
-              <label htmlFor="image" className="relative cursor-pointer  border-4 border-gray-200 text-[#D6D6D7] bg-[#504d4d] rounded-md p-2 ">
-                <input id="image" type="file" onChange={handleFileChange} className="absolute inset-0 w-full h-full opacity-0 cursor-pointer" />
-                {image ? (
-                  <span className="">{image.name}</span>
-                ) : (
-                  <div className="flex">
-                    <MdFileUpload size={24} className="mr-4" />
-                    <span className="font-bold ">Add a cover image</span>
+    <div className="  bg-green-50">
+      <div className="wrapper min-h-screen">
+        <Card className="shadow-sm border-0 mt-2 bg-[#3d3d3e] w-full lg:w-[90%] xl:w-[80%] mx-auto">
+          <CardBody>
+            <Form onSubmit={createPost}>
+              <div className=" lg:pt-24 sm:pt-24 md:pt-24 pt-24 flex justify-end  mr-16">
+                <label htmlFor="image" className="relative cursor-pointer border-4 border-gray-200 text-[#D6D6D7] bg-[#171717] rounded-md p-2 ">
+                  <input id="image" type="file" onChange={handleFileChange} className="absolute inset-0 w-full h-full opacity-0 cursor-pointer" />
+                  {image ? (
+                    <span className="">{image.name}</span>
+                  ) : (
+                    <div className="flex items-center">
+                      <MdFileUpload size={24} className="mr-2" />
+                      <span className="font-bold ">Add a cover image</span>
+                    </div>
+                  )}
+                </label>
+              </div>
+              <div className="my-3 mt-8 ml-4 lg:ml-16 p-4 lg:p-8">
+                <label htmlFor="title" className="text-gray-200 font-xl text-lg mb-4">Title</label>
+                <input
+                  type="text"
+                  id="title"
+                  placeholder="New post title here..."
+                  className="bg-transparent font-medium placeholder-gray-200 hover:placeholder-green-500 focus:border-green-500 appearance-none border-2 w-full py-3 px-3 rounded text-gray-200 leading-tight focus:outline-none"
+                  name="title"
+                  onChange={fieldChanged}
+                  value={post.title}
+                  style={{ wordBreak: 'break-all' }}
+                />
+              </div>
+              <div className="ml-4 lg:ml-16 px-4 lg:px-8">
+                <Input
+                  type="select"
+                  placeholder="Enter here"
+                  className="rounded border-4 border-gray-200 text-[#D6D6D7] focus:outline-none cursor-pointer w-full lg:w-56 bg-[#171717] p-2"
+                  name="category"
+                  onChange={fieldChanged}
+                  value={post.category || ''}
+                  style={{ appearance: 'none' }}
+                >
+                  <option key="0" className="cursor-pointer">Select category</option>
+                  {categories.map((category) => (
+                    <option
+                      key={category._id}
+                      value={category._id}
+                      className="cursor-pointer"
+                    >
+                      {category.categoryName}
+                    </option>
+                  ))}
+                </Input>
+              </div>
+              <div className="my-8 px-4 lg:px-8 ">
+                <h2 htmlFor="content" className="text-gray-200 font-bold flex justify-center py-4">Content</h2>
+                <JoditEditor
+                  className="rounded p-4 lg:p-8 h-96 lg:h-auto"
+                  ref={editor}
+                  value={post.content}
+                  // onBlur={(newContent) => setContent(newContent)} 
+                  onChange={(newContent) => contentFieldChanged(newContent)}
 
-                  </div>
-                )}
-              </label>
-            </div>
-            <div className="my-3 mt-8 ml-16 p-8">
-              <label htmlFor="title" className="text-gray-700 font-xl text-lg mb-8">Title</label>
-              <input
-                type="text"
-                id="title"
-                placeholder="New post title here..."
-                className="bg-transparent font-medium placeholder-gray-500 focus:border-green-500 appearance-none border  w-full py-4 px-3 rounded text-gray-700 leading-tight focus:outline-none"
-                name="title"
-                onChange={fieldChanged}
-                value={post.title}
-                style={{ wordBreak: 'break-all' }}
-              />
-            </div>
-            <div className="ml-16 px-8">
-              <Input
-                type="select"
-                placeholder="Enter here"
-                className="rounded border-4 border-gray-200 text-[#D6D6D7] focus:outline-none cursor-pointer w-56 bg-[#504d4d]  p-2"
-                name="category"
-                onChange={fieldChanged}
-                value={post.category || ''}
-                style={{ appearance: 'none' }}
-              >
-                <option key="0" className="cursor-pointer">Select category</option>
-                {categories.map((category) => (
-                  <option
-                    key={category._id}
-                    value={category._id}
-                    className="cursor-pointer"
-                  >
-                    {category.categoryName}
-                  </option>
-                ))}
-              </Input>
-            </div>
+                />
+              </div>
 
-
-            <div className="my-16 bg-red-800 w-full px-8 ">
-              <h2 htmlFor="content" className="text-white font-bold flex justify-center py-4">Content</h2>
-              <JoditEditor
-                className="rounded p-4 h-96 lg:h-80 xl:h-96"
-                ref={editor}
-                value={post.content}
-                onChange={(newContent) => contentFieldChanged(newContent)}
-              />
-            </div>
-
-
-            <Container className="text-start text-white pb-8">
-              <Button type="submit" className="rounded-lg bg-green-500 font-medium text-lg hover:bg-green-300 p-2">Publish</Button>
-              <Button className="rounded-sm ms-2 text-gray-600 p-2 font-medium hover:rounded-lg hover:bg-green-400 hover:text-white">Save draft</Button>
-            </Container>
-          </Form>
-        </CardBody>
-      </Card>
+              <Container className="text-start text-white pb-8 ml-4 lg:ml-16">
+                <Button type="submit" className="rounded-lg bg-green-500 font-medium text-lg hover:bg-green-300 p-2">Publish</Button>
+                <Button className="rounded-sm ms-2 text-gray-200 p-2 font-medium hover:rounded-lg hover:bg-green-400 hover:text-white">Save draft</Button>
+              </Container>
+            </Form>
+          </CardBody>
+        </Card>
+      </div>
     </div>
+
   );
 }
 

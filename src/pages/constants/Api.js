652106204e69abde80191386408c0f2b78033795
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-04-18 19:43:26 +0100

    Changes were made to constants folder and api file

--- src/pages/constants/Api.js
+++ src/pages/constants/Api.js
@@ -1,3 +1,1 @@
-// export const API_URL='http://localhost:4000/api/v1'     
-// export const API_URL='https://blog-backend-tdjo.onrender.com'     
-export const API_URL = import.meta.env.MODE === "development" ? 'http://localhost:4000' : "https://blog-backend-tdjo.onrender.com"
+   export const API_URL = import.meta.env.MODE === "development" ? 'http://localhost:4000' : "https://blog-backend-tdjo.onrender.com"

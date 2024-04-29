a69da46a9f9e54bc007246a0f39329d7d6dd5da7
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-04-29 13:49:56 +0100

    Changes were made to  NotFoundPage file

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -4,17 +4,17 @@ import { Link } from 'react-router-dom'
 const NotFoundPage = () => {
   return (
     <>
-        <div className='flex justify-center mt-52'>
-            <div className=' w-72 h-auto border-8 p-2 border-green-400 rounded-3xl bg-green-400'>
-                <div className=' bg-black pt-20 h-72 rounded-3xl'>
-                    <h1 className='text-white text-8xl font-bold text-center'>404</h1>
-                </div>
-            </div>
+      <div className='flex justify-center mt-52'>
+        <div className=' w-72 h-auto border-8 p-2 border-green-400 rounded-3xl bg-green-400'>
+          <div className=' bg-black pt-20 h-72 rounded-3xl'>
+            <h1 className='text-white text-8xl font-bold text-center'>404</h1>
+          </div>
         </div>
-                    <p className='text-lg text-semibold mt-6 text-center'>This page does not exist</p>
-                    <Link to={'/'}>
-                    <p className='text-xl font-small text-center mt-8 underline text-green-400'>Return to Home Page</p>
-                    </Link>
+      </div>
+      <p className='text-lg text-semibold mt-6 text-center'>This page does not exist</p>
+      <Link to={'/'}>
+        <p className='text-xl font-small text-center mt-8 underline text-green-400'>Return to Home Page</p>
+      </Link>
     </>
   )
 }

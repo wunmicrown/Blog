258d60c5c64e4a91825fea247571869dbe1a86e5
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-05-11 08:54:05 +0100

    Changes were made to PostList component

--- src/pages/postsFolder/PostsList.jsx
+++ src/pages/postsFolder/PostsList.jsx
@@ -81,7 +81,7 @@ const PostsList = () => {
         <section className="relative bg-white">
           <div className="container px-4 mx-auto">
             <div className="md:max-w-5xl mx-auto mb-10 md:mb-16 text-center">
-              <span className="inline-block py-px px-2 mb-8 text-xs leading-5 text-green-500 bg-green-100 font-medium uppercase rounded-full shadow-sm">
+              <span className="inline-block py-px px-2 mb-6 mt-10 text-xs leading-5 text-green-500 bg-green-100 font-medium uppercase rounded-full shadow-sm">
                 Blog
               </span>
               {/* Trending Articles */}

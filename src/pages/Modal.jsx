733b8e197a0a9c206f841641bd0caf888124769e
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-05-14 17:15:02 +0100

    Changes were made to PostDetails component

--- src/pages/Modal.jsx
+++ src/pages/Modal.jsx
@@ -1,43 +1,71 @@
-// Modal.js
+import { Transition } from '@headlessui/react'
+import axios from 'axios';
+import React, { Fragment } from 'react'
 
-import React from 'react';
-import { FaBlog } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+const Modal = () => {
+  const URL = `${API_URL}/api/v1/posts`;
+  const [showConfirmation, setShowConfirmation] = useState(false);
 
-const Modal = ({ closeModal }) => {
-  return (
-    <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 z-50">
-      <div className="bg-white rounded-lg shadow-lg p-8 max-w-md ">
-        <div className='flex justify-between'>
-          <h2 className="text-2xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-green-600 to-lime-700 hover:from-green-600 hover:to-green-700 focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 rounded-md  transform transition-all duration-500 ease-in-out hover:scale-105 mb-4 text-center text-green-600  flex items-center justify-center">Log in to continue</h2>
-          <button onClick={closeModal} className="text-gray-500 hover:text-gray-700">
-            <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-            </svg>
-          </button>
-        </div>
-        <hr className='mb-8' />
-        <div>
-          <FaBlog className=" text-green-500 h-16 w-20 mt-8 mb-8" />
-
-        </div>
-        <p className="mb-8 text-[16px] font-medium text-black">
-          We're a place where coders share, stay up-to-date and grow their careers.
-        </p>
-        <div className="flex flex-col">
-          <Link to={"/login"}>
-            <button className=" mb-4 p-4 py-3 px-7 w-full leading-6 text-green-50 font-bold text-center bg-gradient-to-r from-green-400 to-blue-600 hover:from-green-500 hover:to-green-700 focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 rounded-md shadow-lg transform transition-all duration-500 ease-in-out hover:scale-105 flex items-center justify-center animate-pulse">
-              Log in
-            </button>
-          </Link>
-          <Link to={"/register"} className='text-center'>
-            <button className=" font-bold bg-clip-text text-transparent bg-gradient-to-r from-green-600 to-lime-700 hover:from-green-600 hover:to-green-700 focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 rounded-md  transform transition-all duration-500 ease-in-out hover:scale-105 mb-4 text-center text-green-600 ">Create account</button>
-
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
+  const closeConfirmationModal = () => {
+    setShowConfirmation(false);
 };
 
-export default Modal;
+  const deletePostHandler = async () => {
+    try {
+        const token = localStorage.getItem('token');
+        if (!token) {
+            console.error('User is not authenticated.');
+            return;
+        }
+        const response = await axios.delete(`${URL}/${postId}`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        if (response.status === 200) {
+            navigate('/profile');
+            toast.error("post delete successfully")
+            setShowConfirmation(false);
+        }
+    } catch (error) {
+        console.error('Error deleting post:', error);
+    }
+};
+  return (
+    <>
+      {/* Modal */}
+      <Transition
+                show={showConfirmation}
+                as={Fragment}
+                enter="transition-opacity duration-200"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="transition-opacity duration-200"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+            >
+                <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50">
+                    <div className="bg-white p-4 rounded-md shadow-md">
+                        <p className="mb-4">Are you sure you want to delete this article?</p>
+                        <div className="flex justify-end">
+                            <button
+                                className="mr-2 bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600"
+                                onClick={deletePostHandler} // Call deletePostHandler to delete the post
+                            >
+                                Delete
+                            </button>
+                            <button
+                                className="bg-gray-200 text-gray-700 px-4 py-2 rounded-md hover:bg-gray-300"
+                                onClick={closeConfirmationModal} // Close modal without deleting
+                            >
+                                Cancel
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </Transition>
+    </>
+  )
+}
+
+export default Modal

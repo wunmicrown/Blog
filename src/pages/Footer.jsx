bf014ad5d009a6cbc990c14e62240f4381aae4d2
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-05-29 12:20:24 +0100

    Chnages were made to Footer component

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <footer className="bg-striped-gradient w-full text-black font-sans font-semibold border-2 fixed bottom-0 border-[#f3f2f2] py-6 mt-auto">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-center items-center">
-            <p className="text-sm text-[#2b2a2a] font-bold ">&copy; 2024 Your Blog App. <span className='text-white'> All rights reserved.</span></p>
+            <p className="text-sm text-[#262525] font-bold ">&copy; 2024 Your Blog App. <span className='text-white'> All rights reserved.</span></p>
             <div className="flex space-x-4">
               {/* <Link to={"#"} className="hover:text-gray-400">About</Link> */}
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,8 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        'striped-gradient': 'linear-gradient(to bottom left, #22C55E 55%, #ffff 45%, #ffff 100%)',
+        'striped-gradient': 'linear-gradient(to bottom left, #22C55E 48%, #ffff 55%, #ffff 40%)',
       },
-      
     },
   },
   plugins: [],

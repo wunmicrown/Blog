497a813ec73457809c9f041e175d107a138e03fa
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-06-07 15:07:45 +0100

    Changes were made to public navbar

--- src/pages/category folder/Category.jsx
+++ src/pages/category folder/Category.jsx
@@ -46,7 +46,7 @@ const Category = () => {
                 />
                 <div className="overlay absolute top-0 left-0 right-0 bottom-0 flex flex-col items-center justify-center bg-black bg-opacity-55 text-white">
                   <h4 className="text-lg font-semibold">{item.category}</h4>
-                  <p className="absolute top-0 left-0 right-0 bottom-0 flex flex-col items-center justify-center bg-black bg-opacity-55 text-white transition-colors duration-500 hover:bg-opacity-65">
+                  <p className="absolute top-0 left-0 right-0 bottom-0 flex flex-col items-center justify-center bg-black bg-opacity-45 text-white transition-colors duration-500 hover:bg-opacity-65">
                     {item.title}
                   </p>
                 </div>
